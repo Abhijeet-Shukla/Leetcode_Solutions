40da6b209dc94f0b6bcf64db4f5810eeaad3b5ef
Abhijeet Shukla <shukla.abhijeet20@gmail.com>
2022-02-18 11:46:01 +0530

    Create MoveZeros.java
    Move Zero LeetCode Java Solution(Easy)

---
+++ MoveZeros.java
@@ -0,0 +1,18 @@
+// Difficulty=Easy
+
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int j=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]!=0){
+                nums[j++]=nums[i];
+            }
+        }
+        while(j<nums.length){
+            nums[j++]=0;
+        }
+    }
+}
+
+// Runtime=2ms
+// Memory=55.2MB
