caf153e7f73474c091a1e1eb157e5f992e8d0273
Abhijeet Shukla <shukla.abhijeet20@gmail.com>
2022-02-22 23:10:53 +0530

    Flood Fill LeetCode Java Solution(Easy)

---
+++ FloodFill.java
@@ -0,0 +1,42 @@
+// Difficulty=Easy
+
+class Solution {
+    public int[][] floodFill(int[][] image, int sr, int sc, int newColor) {
+        int n = image.length;
+        int m = image[0].length;
+        int src_element = image[sr][sc];
+        
+        if(src_element == newColor)
+            return image;
+        
+        Queue<int[]> q=new LinkedList<>();//to store the newly coloured elements's index
+        
+        image[sr][sc]=newColor;
+        
+        q.offer(new int[]{sr,sc});
+        
+        int dx[]={0,0,-1,1};
+        int dy[]={-1,1,0,0};
+        while(!q.isEmpty())
+        {
+            int size=q.size();
+            for(int i=0;i<size;i++)
+            {
+                int p[]=q.poll();
+                for(int j=0;j<4;j++)
+                {
+                    int x=p[0]+dx[j];
+                    int y=p[1]+dy[j];
+                    if(x<0 || y<0 || x>=n || y>=m || image[x][y]!=src_element)
+                        continue;
+                    image[x][y]=newColor;
+                    q.offer(new int[]{x,y});
+                }
+            }
+        }
+        return image; 
+    }
+}
+
+// Runtime=2ms
+// Memory=48.9MB
