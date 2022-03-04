5011afcc70676e926cbe5a8f176e8a397a07be1e
Abhijeet Shukla <shukla.abhijeet20@gmail.com>
2022-03-04 22:36:12 +0530

    Create FindElementinRotatedSortedArray.java

---
+++ FindElementinRotatedSortedArray.java
@@ -0,0 +1,48 @@
+// Difficulty=Medium
+
+class Solution {
+    public int search(int[] nums, int target) {
+        
+        int pivot=findPivot(nums,0,nums.length-1);
+        
+        if(pivot==-1)
+            return binarySearch(nums,0,nums.length-1,target);
+        if(nums[pivot]==target)
+            return pivot;
+        if(nums[0]<=target)
+            return binarySearch(nums,0,pivot-1,target);
+        return binarySearch(nums,pivot+1,nums.length-1,target);
+        
+    }
+      static int findPivot(int arr[], int low, int high)
+    {
+        if (high < low)
+            return -1;
+        if (high == low)
+            return low;
+ 
+        int mid = (low + high) / 2;
+        if (mid < high && arr[mid] > arr[mid + 1])
+            return mid;
+        if (mid > low && arr[mid] < arr[mid - 1])
+            return (mid - 1);
+        if (arr[low] >= arr[mid])
+            return findPivot(arr, low, mid - 1);
+        return findPivot(arr, mid + 1, high);
+    }
+ 
+    static int binarySearch(int arr[], int low, int high, int key)
+    {
+        if (high < low)
+            return -1;
+       int mid = (low + high) / 2;
+        if (key == arr[mid])
+            return mid;
+        if (key > arr[mid])
+            return binarySearch(arr, (mid + 1), high, key);
+        return binarySearch(arr, low, (mid - 1), key);
+    }
+}
+
+//Runtime=1ms
+//Memory=42.8MB
